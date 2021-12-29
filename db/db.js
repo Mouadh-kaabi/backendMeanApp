7ac37eb5c260d85d1740f5f1ec6c835caff4a706
Mouadh-kaabi <kaabi.mouadh@gmail.com>
2021-12-29 18:13:18 +0100

    new push

---
+++ db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const url = "mongodb+srv://applicationSoutras:caca12312312@soutras.2h5dv.mongodb.net/soutras?retryWrites=true&w=majority"
+let mong = mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
